ae2042d282a4fb06a840f2ceea35ea58c49060a9
simjongyeon3 <simjongyeon3@gmail.com>
2023-08-02 18:07:14 +0900

    20230802 6st commit

--- 07/js/addImage-result.js
+++ 07/js/addImage-result.js
@@ -1,16 +1,17 @@
-// 시간정보 취득
+// // 시간정보 취득
 const today = new Date(); 
 const hr = today.getHours();
-const milliseconds = today.getMillisconds();
+const milliseconds = today.getMilliseconds();
 
-// 이미지 출력 지정
+// // 이미지 출력 지정
 const container = document.querySelector("#container");
 
-// 이미지 요소 추가
+// // 이미지 요소 추가
 let newImg = document.createElement("img");
 
-// 오전 오후 에따른 이미지 설정
+// // 오전 오후 에따른 이미지 설정
 newImg.src = (milliseconds < 500) ? "images/morning.jpg" : "images/afternoon.jpg";
+//newImg.src = (hr < 12) ? "images/morning.jpg" : "images/afternoon.jpg";
 
-// 부모 노드에 추가된 이미지 노드 추가
+// // 부모 노드에 추가된 이미지 노드 추가
 container.appendChild(newImg);
